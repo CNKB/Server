5beaad891b68bcd37d402a8e7b879e183b2d6941
user <namsic6460@gmail.com>
2021-09-28 13:36:19 +0900

    Init Project

---
+++ src/main/java/lkd/namsic/cnkb/CnkbApplication.java
@@ -0,0 +1,13 @@
+package lkd.namsic.cnkb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CnkbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CnkbApplication.class, args);
+	}
+
+}
